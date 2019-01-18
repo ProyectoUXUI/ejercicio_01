2b8e92b627e310d268c514d1891bd287d7e627d0
Alumno03 J901 <a03j901@campustls.local>
2019-01-17 20:54:51 -0500

    subo 1er ejercicio js

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.querySelectorAll(".form")[0];
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const cursos = document.querySelectorAll(".cursos")[0];
+        const nombre = document.querySelectorAll(".nombre")[0].value;
+        const apellidos = document.querySelectorAll(".apellidos")[0].value;
+        //const mensaje = `Bienvenido ${nombre.options[nombre.selectedIndex].text}`;
+        const mensaje = `Bienvenido ${nombre}`;
+        const mensajeTexto = document.querySelectorAll(".mensaje-texto")[0];
+
+        mensajeTexto.innerHTML = mensaje;
+
+    });
+});
